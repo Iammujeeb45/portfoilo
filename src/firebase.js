9afde6b3155a52ce5018f738b8b6dd6be5285832
Home <home@Homes-MacBook-Pro.local>
2025-03-07 16:26:54 +0530

    editing

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,5 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDYAW9F1kP29j38dfZuZxK4lppUkSLD25c",
-//   authDomain: "react-portfolio-abhishek.firebaseapp.com",
-//   projectId: "react-portfolio-abhishek",
-//   storageBucket: "react-portfolio-abhishek.appspot.com",
-//   messagingSenderId: "278164302078",
-//   appId: "1:278164302078:web:797fe5960bb12b5fca1daf",
-// };
-
-// export const app = initializeApp(firebaseConfig);
-// export const db = getFirestore();
-
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyC57jYTSrEkqsKYKIkLMYYIV5Y6IYcntd0",
@@ -29,4 +12,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
-
